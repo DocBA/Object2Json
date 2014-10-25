dd3e32313142149404923ed9680218300ee7caf1
DocBA <chuxiaowan@gmail.com>
2014-10-25 18:23:26 +0800

    java 基本类型

---
+++ src/org/jprase/support/BaseType.java
@@ -0,0 +1,32 @@
+package org.jprase.support;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @describe java基本类型
+ * @author missu
+ *
+ */
+public final class BaseType {
+	
+	private byte jbyte;
+	private char jchar;
+	private short jshort;
+	private int jint;
+	private long jlong;
+	private float jfloat;
+	private double jdouble;
+	private boolean jboolean;
+	
+	private Integer jInteger;
+	private Long jLong;
+	private Short jShort;
+	private Byte jByte;
+	private Character jcharacter;
+	private Double jDouble;
+	private Float jFloat;
+	private BigInteger jBigInteger;
+	private BigDecimal jBigDecimal;
+	
+}
